9c4050914bfdac13bafd0f4dccd6adb8e36da52b
Your Name <you@example.com>
2024-08-15 01:04:34 +0530

    file upload

---
+++ 01_variables.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+    int a;
+    a= 567237091;
+    int b;
+    b= 123682346;
+    printf("The multiplication is %d", a*b);
+}
---
+++ 02_data_types.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a = 1;
+    float b;
+    b = 0.69;
+
+    char c;
+    c = 'd';
+
+}
